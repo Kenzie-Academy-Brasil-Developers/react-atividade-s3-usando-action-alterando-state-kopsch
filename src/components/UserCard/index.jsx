1b8a4ee821fa11c95b715f14f09bbc8319d27dc3
thaeki <pedrokopsch@gmail.com>
2021-10-01 13:16:23 -0300

    fix: container name fixed

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -1,9 +1,10 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { changeName } from "../../store/modules/user/action";
 
 const UserCard = () => {
   const [newName, setNewName] = useState("");
+  const name = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   const handleClick = () => dispatch(changeName(newName));
@@ -12,7 +13,7 @@ const UserCard = () => {
 
   return (
     <div>
-      <h3>Name: {newName}</h3>
+      <h3>Name: {name.name}</h3>
       <input placeholder="Insira seu nome" onChange={(e) => handleName(e)} />
       <button onClick={handleClick}>Adicionar</button>
     </div>
